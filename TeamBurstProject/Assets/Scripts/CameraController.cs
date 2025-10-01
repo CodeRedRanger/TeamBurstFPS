f12081fcb86901052f83c4e94e60a20999c06aa8
Diamondskeleton0 <iwilliams21212122@gmail.com>
2025-10-01 16:19:48 -0400

    added camera controller

---
+++ TeamBurstProject/Assets/Scripts/CameraController.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    [SerializeField] int mouseSens;
+    [SerializeField] int vertLimitLow, vertLimitHigh;
+    [SerializeField] bool invertY;
+
+    float rotX;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float mouseX = Input.GetAxisRaw("Mouse X") * mouseSens * Time.deltaTime;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * mouseSens * Time.deltaTime;
+
+        if (invertY)
+        {
+            rotX += mouseY;
+        }
+        else
+        {
+            rotX -= mouseY;
+        }
+
+        rotX = Mathf.Clamp(rotX, vertLimitLow, vertLimitHigh);
+
+        transform.localRotation = Quaternion.Euler(rotX, 0, 0);
+
+        transform.parent.Rotate(Vector2.up, mouseX);
+
+    }
+}
