e900ff5e2201fce68315bc34940aacd9512aba1d
Brian Washington Jr <bw49078@gmail.com>
2025-10-01 07:49:08 -0500

    Created the Player Controller
    The player can now walk with wasd, sprint with shift, and jump with space. There is no camera control. Also changed project settings to allow for both input managers and added the "Sprint" Input in the input manager as well.

---
+++ TeamBurstProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] CharacterController controller;
+
+    [SerializeField] int speed;
+    [SerializeField] int sprintMod;
+    [SerializeField] int jumpSpeed;
+    [SerializeField] int jumpCountMax;
+    [SerializeField] int gravity;
+
+    private Vector3 moveDir;
+    private Vector3 playerVel;
+
+    int jumpCount;
+
+    bool isSprinting;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+
+        Sprint();
+    }
+
+    void Movement()
+    {
+        if (controller.isGrounded)
+        {
+            playerVel = Vector3.zero;
+            jumpCount = 0;
+        }
+        else
+        {
+            playerVel.y -= gravity * Time.deltaTime;
+        }
+
+        moveDir = Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward;
+        controller.Move(moveDir * speed * Time.deltaTime);
+
+        Jump();
+        controller.Move(playerVel * Time.deltaTime);
+    }
+    void Sprint()
+    {
+        if (Input.GetButtonDown("Sprint"))
+        {
+            speed *= sprintMod;
+        }
+        else if (Input.GetButtonUp("Sprint"))
+        {
+            speed /= sprintMod;
+        }
+    }
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpCountMax)
+        {
+            playerVel.y = jumpSpeed;
+            jumpCount++;
+        }
+    }
+}
