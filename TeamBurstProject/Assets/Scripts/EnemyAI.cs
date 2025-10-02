976373d8b3bf04b1b909e0ab8cae67c710d90b9e
ShaylaPillow <TSPillow@student.fullsail.edu>
2025-10-02 11:20:35 -0500

    Created the enemy damage system

--- TeamBurstProject/Assets/Scripts/EnemyAI.cs
+++ TeamBurstProject/Assets/Scripts/EnemyAI.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
+using System.Collections;
 
-public class EnemyAI : MonoBehaviour
+public class EnemyAI : MonoBehaviour, IDamage
 {
-    
+    [SerializeField] Renderer model;
     [SerializeField] int HP;
+    Color colorOrig;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        colorOrig = model.material.color;
         
     }
 
@@ -17,9 +20,24 @@ public class EnemyAI : MonoBehaviour
         
     }
 
-    public void takeDamage(int amount)
+    public void TakeDamage(int amount)
     {
         HP -= amount;
+        
+        if(HP <= 0)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            StartCoroutine(flashRed());
+        }
 
+    }
+    IEnumerator flashRed()
+    {
+        model.material.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        model.material.color = colorOrig;
     }
 }
