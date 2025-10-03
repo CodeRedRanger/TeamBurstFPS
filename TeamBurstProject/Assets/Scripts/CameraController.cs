ed45c82905c85469658dc12d134330ec0560dc6b
Guido <scorpsan@aol.com>
2025-10-02 22:29:54 -0400

    Updated cam controller with Isaac's file

--- TeamBurstProject/Assets/Scripts/CameraController.cs
+++ TeamBurstProject/Assets/Scripts/CameraController.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
 
-public class cameraController : MonoBehaviour
+public class CameraController : MonoBehaviour
 {
-    [SerializeField] int sens;
-    [SerializeField] int lockVertMin, lockVertMax;
+
+    [SerializeField] int mouseSens;
+    [SerializeField] int vertLimitLow, vertLimitHigh;
+
     [SerializeField] bool invertY;
 
     float rotX;
@@ -11,33 +13,34 @@ public class cameraController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //get input
 
-        float mouseX = Input.GetAxisRaw("Mouse X") * sens * Time.deltaTime;
-        float mouseY = Input.GetAxisRaw("Mouse Y") * sens * Time.deltaTime;
+        float mouseX = Input.GetAxisRaw("Mouse X") * mouseSens * Time.deltaTime;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * mouseSens * Time.deltaTime;
 
-        // use invertY   
         if (invertY)
+        {
             rotX += mouseY;
+        }
         else
+        {
             rotX -= mouseY;
+        }
 
-        // clamp camera on x-axis
-        rotX = Mathf.Clamp(rotX, lockVertMin, lockVertMax);
+        rotX = Mathf.Clamp(rotX, vertLimitLow, vertLimitHigh);
 
-
-        //rotate camera on x-axis
         transform.localRotation = Quaternion.Euler(rotX, 0, 0);
 
-        //rotate player on the y-axis
         transform.parent.Rotate(Vector3.up * mouseX);
+
     }
 
 
