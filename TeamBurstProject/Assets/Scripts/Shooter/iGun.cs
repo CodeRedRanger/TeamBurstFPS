6dd78fdc7c35c6dee4118aed688e6010f2107bcd
Tyreek Elam <b3tyreek56@gmail.com>
2025-10-02 20:39:01 -0400

    Update iGun.cs

--- TeamBurstProject/Assets/Scripts/Shooter/iGun.cs
+++ TeamBurstProject/Assets/Scripts/Shooter/iGun.cs
@@ -2,6 +2,16 @@ using UnityEngine;
 
 public interface iGun
 {
+    // Interface for GUN DATA (no logic). A ScriptableObject will implement this.
+    //Any “gun data” asset will implement this, so the shooter can read the same fields no matter the weapon.
 
+    // Unique id for reference.
+    string Id { get; }
+
+    // Core ballistics / damage.
+    float Damage { get; }          // base damage per shot
+    float Range { get; }           // effective range in meters for hitscan; max travel for projectiles (destroy with distance instead of time)
+
+    
 
 }
