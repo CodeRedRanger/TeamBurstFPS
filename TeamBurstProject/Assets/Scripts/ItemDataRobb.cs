e3be5918e4563098d91e5effdc7510b70601e479
Guido <scorpsan@aol.com>
2025-10-06 17:26:12 -0400

    Started hot bar implementation

--- TeamBurstProject/Assets/Scripts/ItemDataRobb.cs
+++ TeamBurstProject/Assets/Scripts/ItemDataRobb.cs
@@ -14,3 +14,21 @@ public class ItemDataRobb : MonoBehaviour
         
     }
 }
+
+/*
+using UnityEngine;
+
+public enum ItemType
+{
+    medkit,
+    grenade
+}
+
+[CreateAssetMenu(menuName = "Items/Item Type")]
+public class ItemData : ScriptableObject
+{
+    public Sprite icon;
+    public ItemType type;
+    public int maxStack;
+}
+*/
