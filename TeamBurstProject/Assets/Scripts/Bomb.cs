0205846646294c147e71c071575790572b45199f
Brian Washington Jr <bw49078@gmail.com>
2025-10-07 18:45:12 -0500

    Changed bomb spawner and bomb script
    The player now has a function to call the bomb spawner on the controller script. Now all bomb spawner does is spawn a bomb at a specified spawn position. Fixed bug where bombs were doing double damage because they were hitting every collider on an enemy.

--- TeamBurstProject/Assets/Scripts/Bomb.cs
+++ TeamBurstProject/Assets/Scripts/Bomb.cs
@@ -1,5 +1,6 @@
-using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class Bomb : MonoBehaviour
 {
@@ -13,25 +14,27 @@ public class Bomb : MonoBehaviour
         StartCoroutine(BombTimer());
     }
 
-
     void Explode()
     {
-
         if (explosionEffect != null)
             Instantiate(explosionEffect, transform.position, Quaternion.identity);
 
-
         Collider[] hitColliders = Physics.OverlapSphere(transform.position, explosionRadius);
+        HashSet<IDamage> damagedEnemies = new HashSet<IDamage>();
+
         foreach (Collider hit in hitColliders)
         {
-            if (hit.CompareTag("Player")) continue; 
+            if (hit.CompareTag("Player")) continue;
 
-            IDamage damageable = hit.GetComponent<IDamage>();
-            if (damageable != null)
+            IDamage damageable = hit.transform.root.GetComponent<IDamage>();
+            if (damageable != null && !damagedEnemies.Contains(damageable))
             {
                 float distance = Vector3.Distance(hit.transform.position, transform.position);
                 if (distance <= explosionRadius)
+                {
                     damageable.TakeDamage(damage);
+                    damagedEnemies.Add(damageable);
+                }
             }
         }
 
--- TeamBurstProject/Assets/Scripts/BombSpawner.cs
+++ TeamBurstProject/Assets/Scripts/BombSpawner.cs
@@ -3,14 +3,9 @@ using UnityEngine;
 public class BombSpawner : MonoBehaviour
 {
     [SerializeField] GameObject bombPrefab;
-    [SerializeField] KeyCode key;
-    void Update()
+
+    public void SpawnBomb(Vector3 spawnPos)
     {
-        if (Input.GetKeyDown(key))
-        {
-            Vector3 spawnPos = gameManager.instance.player.transform.position;
-            spawnPos.y -= gameManager.instance.player.GetComponent<CharacterController>().height / 2f;
-            Instantiate(bombPrefab, spawnPos, Quaternion.identity);
-        }
+        Instantiate(bombPrefab, spawnPos, Quaternion.identity);
     }
 }
