66b7ae96bb008bb35067e26728beb76c8076079c
Guido <scorpsan@aol.com>
2025-10-08 17:45:56 -0400

    Added Library to Level

---
+++ TeamBurstProject/Assets/Scripts/Effects/TriggerDisappearObject.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using System.Collections;
+
+public class TriggerDisappearObject : MonoBehaviour
+{
+    
+
+    [SerializeField] GameObject objectToDisappear;
+    public float delay = 1f; // Delay in seconds before the object appears
+
+    //Options for this script
+    //[SerializeField] string tagToTrigger; 
+    //[SerializeField] bool bossFight;
+    //bool voicePlayed = false;
+
+
+    [SerializeField] AudioClip forBoss;
+
+    void Start()
+    {
+        if (objectToDisappear != null)
+        {
+            objectToDisappear.SetActive(true); // Ensure the object is initially active
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("Player")) // Check if the colliding object has the "Rocket" or Player tag
+        //can make a tag variable if want to use for other things
+        {
+            if (objectToDisappear != null)
+            {
+                objectToDisappear.SetActive(false);
+                //optional if want a delay
+                //StartCoroutine(RemoveObjectAfterDelay(delay)); // Start the coroutine to show the object after a delay
+            }
+        }
+
+        //Use template below to make sound effects occur with disappearance
+        /*
+        if (bossFight == true && other.CompareTag("Player") && voicePlayed == false)
+        {
+            if (forBoss != null)
+            {
+                SoundManager.Instance.PlayEffect(forBoss);
+                voicePlayed = true;
+
+
+            }
+        }*/
+
+    }
+
+    /*
+    IEnumerator RemoveObjectAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        if (objectToDisappear != null)
+        {
+            objectToDisappear.SetActive(false); // Activate the object after the delay
+        }
+    }*/
+}
+}
