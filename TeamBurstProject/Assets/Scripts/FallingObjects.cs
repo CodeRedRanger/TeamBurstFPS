0320e1e4e1ac4e1e01357c2315879c99589c4623
ShaylaPillow <TSPillow@student.fullsail.edu>
2025-10-09 19:48:45 -0500

    updating my branch

--- TeamBurstProject/Assets/Scripts/FallingObjects.cs
+++ TeamBurstProject/Assets/Scripts/FallingObjects.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 
-public class BookshelfFallTrigger : MonoBehaviour
+public class FallingObject : MonoBehaviour
 {
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -13,4 +15,5 @@ public class BookshelfFallTrigger : MonoBehaviour
     {
         
     }
+
 }
