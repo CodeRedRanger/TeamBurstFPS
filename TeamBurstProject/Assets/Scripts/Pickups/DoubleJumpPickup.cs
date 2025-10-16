ebd1911f4533f3d23fe247cbadee3a21b38c7920
SamBoswell-dev <shboswe@gmail.com>
2025-10-15 21:35:25 -0400

    Create Double Jump Prefab

---
+++ TeamBurstProject/Assets/Scripts/Pickups/DoubleJumpPickup.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class DoubleJumpPickup : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
