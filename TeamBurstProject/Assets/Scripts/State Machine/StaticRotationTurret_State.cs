2999a6d90349d6ead7aeb709fe2a36c506c910b9
AssassinSquidGames <campbell.burger.josiah@gmail.com>
2025-11-03 22:00:47 -0600

    Began work on the fixed rotation turret
    Given object rotates between two rotations at a set speed with a pause at max and minimum rotations.

--- TeamBurstProject/Assets/Scripts/State Machine/StaticRotationTurret_State.cs
+++ TeamBurstProject/Assets/Scripts/State Machine/StaticRotationTurret_State.cs
@@ -1,22 +1,41 @@
+using System.Collections;
 using UnityEngine;
 
 public class StaticRotationTurret_State : FiniteState
 {
-    [SerializeField] FiniteState nextState;
-    [SerializeField] GameObject objectToRotate; // DELETE LATER. JUST FOR STATE MACHINE TESTING
+    [SerializeField] FiniteState shootState;
+    [SerializeField] GameObject objectToRotate;
+    [SerializeField] float rotationSpeed;
+    [SerializeField] Vector3 minRotation, maxRotation;
+    [Tooltip("The time that passes before the turret turns the other direction after reaching max or min rotation")]
+    [SerializeField] float pauseDuration;
+    bool rotationPaused = false;
+    float rotationProgress; // between 0 and 1. Closer to 0 = closer to min, closer to 1 = closer to max
+    int currentRotationDirection = 1; // +1 = towardMax, -1 = towardMin
 
     public override void OnUpdate()
     {
         base.OnUpdate();
 
-        RotateObjectClockwise(); // DELETE LATER. JUST FOR STATE MACHINE TESTING
-
-        if (Vector3.Distance(transform.position, gameManager.instance.player.transform.position) >= 12)
-            fsMachine.ChangeToState(nextState);
+        Rotate(); // DELETE LATER. JUST FOR STATE MACHINE TESTING
     }
 
-    private void RotateObjectClockwise() // DELETE LATER. JUST FOR STATE MACHINE TESTING
+    private void Rotate() // DELETE LATER. JUST FOR STATE MACHINE TESTING
     {
-        objectToRotate.transform.localEulerAngles = new Vector3(objectToRotate.transform.eulerAngles.x + 20 * Time.deltaTime, 0, 0);
+        if (rotationPaused) return;
+        rotationProgress = Mathf.Clamp(rotationProgress + rotationSpeed * currentRotationDirection * Time.deltaTime, 0, 1);
+        objectToRotate.transform.localEulerAngles = Vector3.Lerp(minRotation, maxRotation, rotationProgress);
+        if((rotationProgress == 1 && currentRotationDirection == 1) || (rotationProgress == 0 && currentRotationDirection == -1))
+        {
+            currentRotationDirection = -currentRotationDirection;
+            StartCoroutine(PauseCoroutine());
+        }
+    }
+
+    IEnumerator PauseCoroutine()
+    {
+        rotationPaused = true;
+        yield return new WaitForSeconds(pauseDuration);
+        rotationPaused = false;
     }
 }
