df9c5384cfa570ecdcc31b15d032ba484517e320
Tyreek Elam <b3tyreek56@gmail.com>
2025-11-06 23:05:19 -0500

    Spike Trap

---
+++ TeamBurstProject/Assets/Scripts/Effects/tyr-dev/SpikeTraps.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class SpikeTraps : MonoBehaviour
+{
+    [SerializeField] private bool autoTriggered = false, retractOnExit = true, isDud = false;
+
+    public Collider activationZone;
+    public float raiseDelay = 0f, lowerDelay = 0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        // Try to find a trigger collider under this object
+        if (activationZone == null)
+        {
+            var colliders = GetComponentsInChildren<Collider>(true);
+            foreach (var c in colliders)
+            {
+                if (c.isTrigger)
+                {
+                    activationZone = c;
+                    break;
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
