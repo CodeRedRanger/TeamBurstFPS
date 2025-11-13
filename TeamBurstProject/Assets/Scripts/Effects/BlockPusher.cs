a69b43df5f14d2a502fecc52020c8cc1ec8644c7
Guido <scorpsan@aol.com>
2025-11-13 10:25:32 -0500

    altered block pusher

--- TeamBurstProject/Assets/Scripts/Effects/BlockPusher.cs
+++ TeamBurstProject/Assets/Scripts/Effects/BlockPusher.cs
@@ -2,35 +2,29 @@ using UnityEngine;
 
 public class BlockPusher : MonoBehaviour
 {
-    public LayerMask pushLayers;
+    public LayerMask layerTopush;
     public bool canPush;
-    [Range(0.5f, 5f)] public float strength = 1.1f;
+    [Range(0.5f, 5f)] public float power = 2.0f;
 
     void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        //Debug.Log("OnControllerColliderHit: " + hit.collider.name);
-        if (canPush) PushRigidBodies(hit);
+        if (canPush) PushObject(hit);
     }
 
-    private void PushRigidBodies(ControllerColliderHit hit)
+    private void PushObject(ControllerColliderHit hit)
     {
-        // https://docs.unity3d.com/ScriptReference/CharacterController.OnControllerColliderHit.html
 
-        // make sure we hit a non kinematic rigidbody
+
         Rigidbody body = hit.collider.attachedRigidbody;
         if (body == null || body.isKinematic) return;
 
-        // make sure we only push desired layer(s)
         var bodyLayerMask = 1 << body.gameObject.layer;
-        if ((bodyLayerMask & pushLayers.value) == 0) return;
+        if ((bodyLayerMask & layerTopush.value) == 0) return;
 
-        // We dont want to push objects below us
         if (hit.moveDirection.y < -0.3f) return;
 
-        // Calculate push direction from move direction, horizontal motion only
-        Vector3 pushDir = new Vector3(hit.moveDirection.x, 0.0f, hit.moveDirection.z);
+        Vector3 pushDirection = new Vector3(hit.moveDirection.x, 0.0f, hit.moveDirection.z);
 
-        // Apply the push and take strength into account
-        body.AddForce(pushDir * strength, ForceMode.Impulse);
+        body.AddForce(pushDirection * power, ForceMode.Impulse);
     }
 }
