c9df11c506188bb5eddd2bc35ee567f5f7a2170d
Guido <scorpsan@aol.com>
2025-11-14 00:52:19 -0500

    Revert "Merge branch 'dev' into dev-Robb"
    This reverts commit bbf86ae79e38f304b173ad93a918fc5a8f3dda3f, reversing
    changes made to 26bb905d72207724aa8eab8fb3a5e072bce284f5.

--- TeamBurstProject/Assets/Scripts/Player/PlayerController.cs
+++ TeamBurstProject/Assets/Scripts/Player/PlayerController.cs
@@ -509,17 +509,13 @@ public class PlayerController : MonoBehaviour, IDamage, IPickupGun, IPickupKey
     //*********************** Gun Methods **********************
     void reload()
     {
-        if (gunList.Count <= 0) return;
-
         if (Input.GetButtonDown("Reload"))
         {
-            if (gunList[gunListPos].ammoCur < gunList[gunListPos].ammoMax)
-                Reticle.instance.PlayReload();
-
             gunList[gunListPos].ammoCur = gunList[gunListPos].ammoMax;
             //I added to lecture code
             updatePlayerUI();
 
+            Reticle.instance.PlayReload();
         }
     }
 
