09c3d3eb783297144be7e67a0e123f67737b066b
RamiAbdu24 <rabdu@student.fullsail.edu>
2025-11-14 00:50:14 -0500

    lava script fix

--- TeamBurstProject/Assets/Scripts/In Progress/Lava.cs
+++ TeamBurstProject/Assets/Scripts/In Progress/Lava.cs
@@ -8,25 +8,13 @@ public class Lava : MonoBehaviour
     [SerializeField] float scrollSpeedY;
     [SerializeField] bool scrollTexture = true;
 
-    //[SerializeField] AudioSource audioSource;
-    //[SerializeField] AudioClip lavaClip;
-    [SerializeField] ParticleSystem PS;
-
     Renderer rend;
     Vector2 offset;
-    ParticleSystem.ShapeModule shape;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rend = GetComponent<Renderer>();
         offset = rend.material.mainTextureOffset;
-
-        shape = PS.shape;
-        shape.scale = new Vector3(transform.localScale.x - 0.5f, transform.localScale.z - 0.5f, transform.localScale.y);
-        PS.Play();
-
-        //audioSource.clip = lavaClip;
-        //audioSource.Play();
     }
 
     // Update is called once per frame
