e4a91e301d7a80b88c209ddc8cc4885a5e3620ea
Guido <scorpsan@aol.com>
2025-11-15 20:41:10 -0500

    Added hand to pistol

--- TeamBurstProject/Assets/Scripts/Camera/CameraController.cs
+++ TeamBurstProject/Assets/Scripts/Camera/CameraController.cs
@@ -22,8 +22,20 @@ public class CameraController : MonoBehaviour
     void Update()
     {
 
+        //Get raw mouse input
+        //float mouseX_raw = Input.GetAxis("Mouse X");
+        //float mouseY_raw = Input.GetAxis("Mouse Y");
+
+        //Can make variable public float maxDeltaPerFrame = 10;f; 
+        //mouseX_raw = Mathf.Clamp(mouseX_raw, -10, 10);
+        //mouseY_raw = Mathf.Clamp(mouseY_raw, -10, 10);
+
+
+
         float mouseX = Input.GetAxisRaw("Mouse X") * mouseSens * Time.deltaTime;
+        //float mouseX = mouseX_raw * mouseSens * Time.deltaTime;
         float mouseY = Input.GetAxisRaw("Mouse Y") * mouseSens * Time.deltaTime;
+        //float mouseY = mouseY_raw * mouseSens * Time.deltaTime;
 
         if (invertY)
         {
